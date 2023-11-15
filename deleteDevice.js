110c6c6732053893bd1119fb68369c0deaad2742
aulianafahrian <aulianafahrian@gmail.com>
2023-11-15 13:17:42 +0700

    Add deleteDevice function with confirmation dialog and authorization header

---
+++ deleteDevice.js
@@ -0,0 +1,47 @@
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+const deleteDevice = async (IDHAPUS) => {
+    const deviceId = IDHAPUS;
+    console.log(`device id= ${deviceId}`);
+
+    const isConfirmed = await Swal.fire({
+        title: "Apakah Anda yakin ingin menghapus device ini?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#6750A4",
+        cancelButtonColor: "#999999",
+        confirmButtonText: "Ya, Hapus!",
+        cancelButtonText: "Batal",
+    });
+
+    if (isConfirmed.isConfirmed) {
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", `Bearer ${getCookie("token")}`);
+
+        const targetUrl = `https://asia-southeast2-urse-project.cloudfunctions.net/urse-deletedevice?id=${deviceId}`;
+
+        try {
+            const response = await fetch(targetUrl, {
+                method: "DELETE",
+                headers: myHeaders,
+                redirect: "follow",
+            });
+
+            if (response.ok) {
+                await Swal.fire({
+                    icon: "success",
+                    title: "Data berhasil dihapus",
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+                location.reload();
+            } else {
+                throw new Error(`Request failed with status: ${response.status}`);
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    }
+};
+
+window.deleteDevice = deleteDevice;
