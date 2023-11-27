dc757d196f02e472173a902bb7ebed518cf15a07
erditona <erditonaushaadam@gmail.com>
2023-11-27 21:05:44 +0700

    Refactor logout function to remove unnecessary data

--- logout.js
+++ logout.js
@@ -1,23 +1,26 @@
 const logout = () => {
-    Swal.fire({
-        title: "Apa kah kamu yakin?",
-        text: "Sesi kamu akan berakhir!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#6750A4",
-        cancelButtonColor: "#999999",
-        confirmButtonText: "Logout",
-    }).then((result) => {
-        if (result.isConfirmed) {
-            // localStorage.removeItem("token");
-            // Menghapus url sebelumnya dari local storage
-            localStorage.removeItem("previousPageURL");
-            // Menghapus toke dari cookie
-            document.cookie = "token= ; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  Swal.fire({
+    title: "Apa kah kamu yakin?",
+    text: "Sesi kamu akan berakhir!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#6750A4",
+    cancelButtonColor: "#999999",
+    confirmButtonText: "Logout",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      // localStorage.removeItem("token");
+      // Menghapus data dari local storage
+      localStorage.removeItem("previousPageURL");
+      localStorage.removeItem("totalDevices");
+      localStorage.removeItem("userEmail");
+      localStorage.removeItem("userlocation");
+      // Menghapus toke dari cookie
+      document.cookie = "token= ; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 
-            window.location.href = "../../index.html";
-        }
-    });
-}
+      window.location.href = "../../index.html";
+    }
+  });
+};
 
 window.logout = logout;
